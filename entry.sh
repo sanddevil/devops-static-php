30852492308de1cf369f1e2f3d35866e70be7005
Nigel Milligan <grangeosands@googlemail.com>
2020-01-14 12:52:32 +0000

    Added debug trace for mount point

--- entry.sh
+++ entry.sh
@@ -2,6 +2,16 @@
 
 set +e
 
+while [ ! -d /var/static_tests/web ]
+do
+  sleep 2
+  echo "Waiting for volume to be mounted and files copied into position"
+done
+
+ls -las /var/static_tests
+ls -las /var/static_tests/web
+
+
 phpcpd /var/static_tests/web/core/modules/views/src
 phpcs /var/static_tests/web/core/modules/views/src
 phplint /var/static_tests/web/core/modules/views/src
