da02921c45bb9245c57019f4248e4f3033ba5b37
Nigel Milligan <grangeosands@googlemail.com>
2020-01-14 12:58:40 +0000

    Removed debug trace

--- entry.sh
+++ entry.sh
@@ -4,13 +4,9 @@ set +e
 
 while [ ! -d /var/static_tests/web ]
 do
-  sleep 2
+  sleep 5
   echo "Waiting for volume to be mounted and files copied into position"
 done
-
-ls -las /var/static_tests
-ls -las /var/static_tests/web
-
 
 phpcpd /var/static_tests/web/core/modules/views/src
 phpcs /var/static_tests/web/core/modules/views/src
